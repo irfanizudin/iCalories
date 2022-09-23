9b07a97a07f0ac3ac828224f1b995316415d20dd
irfanizudin <irfanizzudyn@gmail.com>
2022-09-23 08:08:35 +0700

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Irfan Izudin on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
